49297c0ccc9ee87a4ef6244a07573d4f77931c99
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-04 15:28:39 +0530

    Add files via upload

---
+++ C++/exception handling.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x = -1;
+cout<< "Before try \n";
+try
+{
+cout<< "Inside try \n";
+if (x < 0)
+{
+throw x;
+cout<< "After throw (Never executed) \n";
+}
+}
+catch (int x )
+{
+cout<< "Exception Caught \n";
+}
+cout<< "After catch (Will be executed) \n";
+return 0;
+}
+
+
+
