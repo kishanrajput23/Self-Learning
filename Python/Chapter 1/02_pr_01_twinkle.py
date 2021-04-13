1830554b857047948ae3457533e3fa739904d32a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-13 13:27:34 +0530

    problem 1 twinkle.py

---
+++ Python/Chapter 1/02_pr_01_twinkle.py
@@ -0,0 +1,10 @@
+'''
+This problem is a solution of
+Problem 1 of CodeWithHarry Practice Set!
+'''
+# This is also a comment just like the above line
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
