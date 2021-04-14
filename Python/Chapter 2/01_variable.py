758a62111c37cf1c607a67ba3152db40326cb75e
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-14 20:35:11 +0530

    working with variables

---
+++ Python/Chapter 2/01_variable.py
@@ -0,0 +1,19 @@
+a_122 = '''harry'''
+# a = 'harry'
+# a = "harry"
+b = 345
+c = 45.32
+d = True
+# d = None
+
+# Printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
