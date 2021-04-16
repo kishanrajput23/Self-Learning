7b22d7c23cc95d372a3bb4ca24d2341c9a94545f
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-16 17:48:43 +0530

    string slicing

---
+++ Python/Chapter 3/02_string_slicing.py
@@ -0,0 +1,21 @@
+# greeting = "Good Morning, "
+# name = "Harry"
+# print(type(name))
+
+# Concatenating two strings
+# c = greeting + name
+# print(c)
+name = "Harry"
+# print(name[4])
+# name[3] = "d" --> Does not work
+
+# print(name[1:4])
+# print(name[:4]) # is same as name[0:4]
+# print(name[1:]) # is same as name[1:5]
+# c = name[-4:-1] # is same is name[1:4]
+# print(c)
+
+name = "HarryIsGood"
+# d = name[0::3]
+d = name[:0:-1]
+print(d)
