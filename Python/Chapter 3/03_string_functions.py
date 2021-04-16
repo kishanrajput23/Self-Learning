4e820f2e8ecca7770157335a5d377dbfbd5aaf42
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-16 17:49:20 +0530

    str() functions

---
+++ Python/Chapter 3/03_string_functions.py
@@ -0,0 +1,10 @@
+story = "once upon a time there was a youtuber named Harry who uploaded python course with notes Harry"
+
+# String Functions
+# print(len(story))
+# print(story.endswith("notes"))
+# print(story.count("c"))
+# print(story.capitalize())
+# print(story.find("upon"))
+print(story.replace("Harry", "CodeWithHarry"))
+ 
