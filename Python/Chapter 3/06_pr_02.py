23f3b96c78449a52b499ee43ce8ad59c6beb92a3
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-18 14:11:33 +0530

    problem 2 filling letter template

---
+++ Python/Chapter 3/06_pr_02.py
@@ -0,0 +1,13 @@
+letter = '''Dear <|NAME|>,
+Greetings from ABC coding house. I am happy to tell you about your selection
+You are selected!
+Have a great day ahead!
+Thanks and regards,
+Bill
+Date: <|DATE|>
+'''
+name = input("Enter Your Name\n")
+date = input("Enter Date\n")
+letter = letter.replace("<|NAME|>", name)
+letter = letter.replace("<|DATE|>", date)
+print(letter)
