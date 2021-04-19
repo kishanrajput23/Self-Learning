6ed2d03efca072da00ec66347830eb9a05ae5ccb
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-19 20:09:37 +0530

    lists

---
+++ Python/Chapter 4/01_lists.py
@@ -0,0 +1,17 @@
+# Create a list using []
+a = [1, 2 , 4, 56, 6]
+
+# Print the list using print() function
+print(a)
+
+# Access using index using a[0], a[1], a[2]
+print(a[2])
+
+# Change the value of list using
+a[0] = 98
+print(a)
+
+# We can create a list with items of different types
+c = [45, "Harry", False, 6.9]
+print(c)
+
