254277576cd34bbfa6d537faa3f6e28460fa24a3
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-20 14:27:29 +0530

    tuples

---
+++ Python/Chapter 4/04_tuples.py
@@ -0,0 +1,13 @@
+# Creating a tuple using ()
+t = (1, 2, 4, 5)
+
+# t1 = () # Empty tuple
+# t1 = (1) # Wrong way to declare a Tuple with Single element
+t1 = (1,) # Tuple with Single element
+print(t1)
+
+# Printing the elements of a tuple
+# print(t[0])
+
+# Cannot update the values of a tuple
+# t[0] = 34 # throws an error
