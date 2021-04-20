fc6b14b1230551cd2e32f16e1db5121226282283
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-20 14:26:29 +0530

    list methods

---
+++ Python/Chapter 4/03_list_methods.py
@@ -0,0 +1,9 @@
+l1 = [1, 8, 7, 2, 21, 15]
+print(l1)
+# l1.sort() # sorts the list
+# l1.reverse() # reverses the list
+#l1.append(45) # adds 45 at the end of the list 
+# l1.insert(2, 544) # inserts 544 at index 2
+# l1.pop(2) # removes element at index 2
+l1.remove(21) # removes 21 from the list
+print(l1)
