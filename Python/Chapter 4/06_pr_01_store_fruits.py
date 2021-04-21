b131507bbb115a5c02b6481fc764c0a8f73cdcbd
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-21 16:59:01 +0530

    problem 1 storing fruits in list

---
+++ Python/Chapter 4/06_pr_01_store_fruits.py
@@ -0,0 +1,10 @@
+f1 = input("Enter Fruit Number 1: ")
+f2 = input("Enter Fruit Number 2: ")
+f3 = input("Enter Fruit Number 3: ")
+f4 = input("Enter Fruit Number 4: ")
+f5 = input("Enter Fruit Number 5: ")
+f6 = input("Enter Fruit Number 6: ")
+f7 = input("Enter Fruit Number 7: ")
+
+myFruitList = [f1, f2, f3, f4, f5, f6, f7]
+print(myFruitList)
