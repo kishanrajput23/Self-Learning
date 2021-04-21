0e4c0d4994a3112e8e2d5cab0260bd82d44c02d9
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-21 16:59:48 +0530

    problem 2 displaying marks

---
+++ Python/Chapter 4/07_pr_02_marks.py
@@ -0,0 +1,10 @@
+m1 = int(input("Enter Marks for Student Number 1: "))
+m2 = int(input("Enter Marks for Student Number 2: "))
+m3 = int(input("Enter Marks for Student Number 3: "))
+m4 = int(input("Enter Marks for Student Number 4: "))
+m5 = int(input("Enter Marks for Student Number 5: "))
+m6 = int(input("Enter Marks for Student Number 6: "))
+
+myList = [m1, m2, m3, m4, m5, m6]
+myList.sort()
+print(myList)
