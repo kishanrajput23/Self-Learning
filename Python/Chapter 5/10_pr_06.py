89cab85230c9bf398467defb77d91e6315167799
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-04-28 12:53:54 +0530

    problem 6 creating dict with some key-value pair

---
+++ Python/Chapter 5/10_pr_06.py
@@ -0,0 +1,11 @@
+favLang = {}
+a = input("Enter your favorite language Shubham\n")
+b = input("Enter your favorite language Ankit\n")
+c = input("Enter your favorite language Sonali\n")
+d = input("Enter your favorite language Harshita\n")
+favLang['shubham'] = a
+favLang['ankit'] = b
+favLang['sonali'] = c
+favLang['harshita'] = d
+
+print(favLang)
