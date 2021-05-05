8f93dd4123f159df1793e5cfc763b67b120a5788
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-05-05 12:38:07 +0530

    finding given name has less than 10 char or not

---
+++ Python/Chapter 6/11_pr_04.py
@@ -0,0 +1,5 @@
+username = input("Enter your username\n")
+if len(username) < 10:
+    print("Yes username contains less than 10 characters")
+else:
+    print("No your username does not contain less than 10 characters")
