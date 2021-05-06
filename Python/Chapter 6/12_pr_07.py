1e7860ca3707ee6d8b0d64bfa3791b34fe9f5243
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-05-06 14:30:33 +0530

    finding particular string present in given string

---
+++ Python/Chapter 6/12_pr_07.py
@@ -0,0 +1,49 @@
+post = input("enter your post\n")
+
+if post.find("harry") == -1:
+    print("NOT FOUND")
+
+elif post.find("Harry") == -1:
+    print("NOT FOUND")
+
+elif post.find("HArry") == -1:
+    print("NOT FOUND")
+
+elif post.find("HARry") == -1:
+    print("NOT FOUND")
+
+elif post.find("HARRy") == -1:
+    print("NOT FOUND")
+
+elif post.find("HARRY") == -1:
+    print("NOT FOUND")
+
+elif post.find("HarRY") == -1:
+    print("NOT FOUND")
+
+elif post.find("HarRy") == -1:
+    print("NOT FOUND")
+
+elif post.find("HaRry") == -1:
+    print("NOT FOUND")
+
+elif post.find("HaarY") == -1:
+    print("NOT FOUND")
+
+elif post.find("haRRy") == -1:
+    print("NOT FOUND")
+
+elif post.find("harrY") == -1:
+    print("NOT FOUND")
+
+elif post.find("hARRy") == -1:
+    print("NOT FOUND")
+
+elif post.find("HaRRy") == -1:
+    print("NOT FOUND")
+
+elif post.find("HARRy") == -1:
+    print("NOT FOUND")
+else:
+    print("YAY FOUND IT")
+
