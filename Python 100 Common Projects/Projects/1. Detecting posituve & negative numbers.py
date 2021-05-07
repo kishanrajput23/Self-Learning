778f64b2528de17d3cafb1d4cce792f2f5781d9b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-05-07 12:54:41 +0530

    Project 1

---
+++ Python 100 Common Projects/Projects/1. Detecting posituve & negative numbers.py
@@ -0,0 +1,8 @@
+x = float(input("Insert any number: "))
+
+if x>0:
+    print("This is a POSITIVE number")
+elif x < 0:
+    print("This is a NEGATIVE number")
+else:
+    print("The number is ZERO")
