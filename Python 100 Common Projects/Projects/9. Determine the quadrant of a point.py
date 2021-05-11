d78e06507e8e94e0e3e0770da38dd33e76ae0147
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-05-11 14:28:27 +0530

    Project 9

--- Python 100 Common Projects/Projects/9. Determine the quadrant of a point.py
+++ Python 100 Common Projects/Projects/9. Determine the quadrant of a point.py
@@ -3,11 +3,11 @@ y = float(input("Insert coordinate of point y: "))
 
 if x > 0 and y > 0:
     print("The first quadrant")
-elif x < 0 and y >0:
+elif x < 0 and y > 0:
     print("The second quadrant")
 elif x < 0 and y  < 0:
     print("The third quadrant")
-elif x < 0 and y < 0:
+elif x > 0 and y < 0:
     print("The forth quadrant")
 elif x == 0 and y == 0:
     print("Point of origin")
