a337bae82a3cc81a50548a1572765119b6208381
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-06-06 20:44:35 +0530

    Problem 1 finding greate

---
+++ Python/Chapter 8/05_pr_01.py
@@ -0,0 +1,14 @@
+def maximum(num1, num2, num3):
+    if (num1>num2):
+        if(num1>num3):
+            return num1
+        else:
+            return num3
+    else:
+        if(num2>num3):
+            return num2
+        else:
+            return num3
+
+m = maximum(13, 55, 2)
+print("The value of the maximum is " + str(m))
