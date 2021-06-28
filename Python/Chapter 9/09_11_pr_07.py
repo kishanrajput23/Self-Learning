69a4a788b3e125caf33b7da305fc5a46c41e0d8b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-06-28 18:02:21 +0530

    problem 7 finding line where word found in a file

---
+++ Python/Chapter 9/09_11_pr_07.py
@@ -0,0 +1,9 @@
+content = True
+i = 1
+with open("log.txt") as f:
+    while content:
+        content = f.readline()
+        if 'python' in content.lower():
+            print(content)
+            print(f"Yes python is present on line number {i}") 
+        i+=1
