743282f4431eab5f596d5e612eb8dea657556466
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-07-14 23:19:24 +0530

    railway registration using oops

---
+++ Python/Chapter 10/02_railway_oops.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+pd.DataFrame()
+class RailwayForm:
+    formType = "RailwayForm"
+    def printData(self):
+        print(f"Name is {self.name}")
+        print(f"Train is {self.train}")
+
+harrysApplication = RailwayForm()
+harrysApplication.name = "Harry"
+harrysApplication.train = "Rajdhani Express"
+harrysApplication.printData()
