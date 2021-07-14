70b39b1e3a30a64fc50f10f995813f5aafdda8f6
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-07-14 23:18:45 +0530

    intro to oops

---
+++ Python/Chapter 10/01_oops.py
@@ -0,0 +1,22 @@
+class Number:
+    def sum(self):
+        return self.a + self.b
+
+num = Number()
+num.a = 12
+num.b = 34
+s = num.sum()
+print(s)
+
+# a = 12
+# b = 34
+
+# print("The sum of a and b is ", a+b)
+
+'''
+PascalCase 
+EmployeeName -->PascalCase 
+
+camelCase
+isNumeric, isFloatOrInt -->camelCase
+'''
