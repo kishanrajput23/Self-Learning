c1bb28b5ecf325f763d8a7af87cc696dac309711
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-07-26 19:41:03 +0530

    using constructor

---
+++ Python/Chapter 10/08_constructor.py
@@ -0,0 +1,28 @@
+class Employee:
+    company = "Google"
+
+    def __init__(self, name, salary, subunit):
+        self.name = name
+        self.salary = salary
+        self.subunit = subunit
+        print("Employee is created!") 
+
+    def getDetails(self):
+        print(f"The name of the employee is {self.name}")
+        print(f"The salary of the employee is {self.salary}")
+        print(f"The subunit of the employee is {self.subunit}")
+
+    def getSalary(self, signature):
+        print(f"Salary for this employee working in {self.company} is {self.salary}\n{signature}")
+
+    @staticmethod
+    def greet():
+        print("Good Morning, Sir")
+
+    @staticmethod
+    def time():
+        print("The time is 9AM in the morning")
+
+harry = Employee("Harry", 100, "YouTube")
+# harry = Employee() --> This throws an error (missing 3 required positional arguments:)
+harry.getDetails()
