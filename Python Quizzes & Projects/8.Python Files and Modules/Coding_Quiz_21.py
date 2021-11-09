ddbb58a6bcacff823b85f7986bd7ed2665f220f5
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-11-09 22:18:02 +0530

    Coding Quiz 21

---
+++ Python Quizzes & Projects/8.Python Files and Modules/Coding_Quiz_21.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def even_or_odd(n):
+      if n % 2 == 0:
+        print('This number is even')
+      else:
+        print('This number is odd')
+
+
+# In[4]:
+
+
+even_or_odd(3)
+
+
+# In[6]:
+
+
+even_or_odd(10)
+
+
+# In[ ]:
+
+
+
+
---
+++ Python Quizzes & Projects/8.Python Files and Modules/square_area.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Create a python module contain python code used to calculate the area of a square shape,
+# 
+# # then use this module in another python file to calculate the area of a square has the side equal 20 cm :
+
+# In[ ]:
+
+
+def square(n):
+    return n* n
+
