c1c117f9c880cf9ee82daac32aac1600cb6176ae
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-11-09 22:17:24 +0530

    Files & Modules

---
+++ Python Quizzes & Projects/8.Python Files and Modules/Python+Data+Modules.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#So Firstly we save the last as check.py on our desktop, then we will import this module in new python.py:
+#We will proceed as follow:
+import check
+answer = check.even_or_odd(3)
+
+
+# In[6]:
+
+
+answer = check.even_or_odd(6)
+
+
+# In[7]:
+
+
+answer = check.even_or_odd(54665)
+
+
+# In[ ]:
+
+
+
+
