8d9e41d5a577e683e04a7d34ddbb9a06e29c6d98
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-11-14 19:42:21 +0530

    problems on virtual env, lambda, join, map, filter

---
+++ Python/Chapter 13/04_format.py
@@ -0,0 +1,10 @@
+name = "Harry"
+channel = "CodeWithHarry"
+type = "Coding"
+# a = f"This is {name}"
+# a = "This is {}".format(name)
+# a = "This is {} and his channel is {}".format(name, channel)
+# a = "This is {0} and his {2} channel is {1}".format(name, channel, type)
+a = "This is {} and his {} channel is {}".format(name, channel, type)
+
+print(a)
