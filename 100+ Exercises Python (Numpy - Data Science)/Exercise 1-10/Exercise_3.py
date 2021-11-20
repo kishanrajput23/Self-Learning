2ae23975b1b9dc5a8a3987f82c6d14c55c8a1f3b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-11-20 23:37:45 +0530

    Exercise 3 Solution

---
+++ 100+ Exercises Python (Numpy - Data Science)/Exercise 1-10/Exercise_3.py
@@ -0,0 +1,15 @@
+import numpy as np
+ 
+A = np.array([[0, 0, 0],
+              [0, 0, 0]])
+ 
+B = np.array([[0, 0, 0],
+              [0, 1, 0]])
+ 
+C = np.array([[False, False, False],
+              [True, False, False]])
+ 
+D = np.array([[0.1, 0.0]])
+ 
+for name, array in zip(list('ABCD'), [A, B, C, D]):
+    print(f'{name}: {np.any(array)}')
