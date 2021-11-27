85429a81411cff51c548d785b5f841081361429c
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-11-27 21:14:31 +0530

    Problem 97

--- 100 Python Challenge/Classes/97. person class.py
+++ 100 Python Challenge/Classes/97. person class.py
@@ -20,3 +20,26 @@ p2.compare(p1) ➞ "Nancy is older than me."
 
 p1.compare(p3) ➞ "Allen is the same age as me."
 '''
+
+class Person:
+    def _init_(self,name,age):
+        self.name=name
+        self.age=age
+    def compare(self,val):
+        if self.age>val.age:
+            print(f"{val.name} is younger than me.")
+        elif self.age<val.age:
+            print(f"{val.name} is older than me.")
+        elif self.age==val.age:
+            print(f"{val.name} is the same age as me.")
+
+p1 = Person("Nancy", 45)
+
+p2 = Person("Maya", 36)
+
+p3 = Person("Allen", 45)
+
+
+p1.compare(p2)
+p2.compare(p1)
+p1.compare(p3)
