dc994966c245a9bb9483313e392f83f413340cd5
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-11-27 20:56:58 +0530

    Problem 97

---
+++ 100 Python Challenge/Classes/97. person class.py
@@ -0,0 +1,22 @@
+'''
+Create a method in the Person class which returns how another person's age compares. 
+Given the objects p1, p2 and p3, which will be initialized with the attributes name and age, return a sentence in the following format: {other_person} is {older than / younger than / the same age as} me.
+
+Examples :
+
+p1 = Person("Nancy", 45)
+
+p2 = Person("Maya", 36)
+
+p3 = Person("Allen", 45)
+
+
+
+Expected output:
+
+p1.compare(p2)  ➞ "Maya is younger than me."
+
+p2.compare(p1) ➞ "Nancy is older than me."
+
+p1.compare(p3) ➞ "Allen is the same age as me."
+'''
