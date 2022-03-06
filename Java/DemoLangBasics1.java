ed0b332457cfa0a8e159417287f6511922601045
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-06 22:24:17 +0530

    DemoLangBasics1

---
+++ Java/DemoLangBasics1.java
@@ -0,0 +1,16 @@
+public class DemoLangBasics1 {
+	public static void main(String[] args) {
+                                // TODO Auto-generated method stub
+		int x=-2;
+		if(x==0) {
+			System.out.println("zero");
+		}
+		else if(x%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+}
+    
