b71ceccad318bb430309472b8bbccbe44fa4d017
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-06 22:28:12 +0530

    DemoLangBasics3

---
+++ Java/DemoLangBasics3.java
@@ -0,0 +1,8 @@
+public class DemoLangBasics {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		System.out.println(x);
+	}
+}
