36a630f93702876f69f23b3ca93f0120b3b2a624
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-06 22:29:47 +0530

    Switch

---
+++ Java/Switch.java
@@ -0,0 +1,18 @@
+public class Switch {
+	public static void main(String[] args) {
+		int x =4;
+		switch(x){
+			case 1:System.out.println("1");
+				break;
+			case 2:System.out.println("2");
+		       		break;
+			case 3:System.out.println("3");
+		       		break;
+			case 4:System.out.println("4");
+		       		break;
+		default:System.out.println("default");
+			break;		
+		}
+	}
+	
+}
