d13870fe6aa107ca94da6b8645cd377435107280
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-06 22:26:07 +0530

    DemoLangBasics2

---
+++ Java/DemoLangBasics2.java
@@ -0,0 +1,17 @@
+public class DemoLangBasics {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=-15, y=9;
+		if(x>0 ) {
+			if(x==15) {
+				System.out.println("equal to 15");
+			}
+			else {
+				System.out.println("greater than 0 and not 15");
+			}
+		}
+		else {
+			System.out.println("outside if"); 
+		}
+	}
+}
