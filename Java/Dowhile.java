83c181f34798fbee372d4ef647c09174360ff6d9
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-07 19:13:14 +0530

    Dowhile

---
+++ Java/Dowhile.java
@@ -0,0 +1,13 @@
+public class Dowhile {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0;
+		
+		 do {
+		System.out.println(x);
+		x++;
+		
+		}while(x>20);
+		
+	}
+}
