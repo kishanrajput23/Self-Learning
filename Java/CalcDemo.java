8fc08193993f136616c90449f8cfa93be034ae61
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-07 19:18:57 +0530

    Calc

---
+++ Java/CalcDemo.java
@@ -0,0 +1,15 @@
+public class CalcDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c1 = new Calculator();
+		c1.x=10;
+		c1.y=9;
+		c1.add();
+		c1.mul();
+		Calculator c2 = new Calculator();
+		c2.x=90;
+		c2.y=8;
+		c2.add();
+		
+	}
+}
