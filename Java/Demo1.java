16eedafdf5e2a28420e30ab317ca027f48b0b1a4
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-07 19:16:17 +0530

    break example

---
+++ Java/Demo1.java
@@ -0,0 +1,12 @@
+public class Demo1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<10;i++) {
+			if(i==3) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+}
