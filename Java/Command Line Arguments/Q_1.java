5e3810baa713b9b657494fed8197e31249374502
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-04-25 23:48:08 +0530

    Delete Java/Command Line Arguments directory

--- Java/Command Line Arguments/Q_1.java
+++
@@ -1,7 +0,0 @@
-public class Q_1 {
-
-    public static void main(String args[]) {
-        System.out.println(args[0] + " Technologies " + args[1]);
-    }
-    
-}
--- Java/Command Line Arguments/Q_2.java
+++
@@ -1,7 +0,0 @@
-public class Q_2 {
-
-    public static void main(String args[]) {
-        System.out.println("Welcome " + args[0] + "!!");
-        
-    }
-}
