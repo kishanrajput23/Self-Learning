712ddc3d5bff5ed3e287112443cdb94a56f3e003
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-04-25 23:49:58 +0530

    DemoLangBasics4

---
+++ Java/DemoLangBasics4.java
@@ -0,0 +1,14 @@
+public class DemoLangBasics4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Internship";
+		
+		if(s1.equalsIgnoreCase("internship")) {
+			System.out.println("match");
+		}
+		
+		else {
+			System.out.println("not matched");
+		}
+	}
+}
